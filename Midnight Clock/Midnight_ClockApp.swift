afade5b99195d450c70733277ee3e4283ae31746
joaorato <jpedrorato33@gmail.com>
2025-10-02 21:38:39 +0100

    Initial Commit

---
+++ Midnight Clock/Midnight_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Midnight_ClockApp.swift
+//  Midnight Clock
+//
+//  Created by João Rato on 02/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Midnight_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Midnight ClockTests/Midnight_ClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  Midnight_ClockTests.swift
+//  Midnight ClockTests
+//
+//  Created by João Rato on 02/10/2025.
+//
+
+import Testing
+@testable import Midnight_Clock
+
+struct Midnight_ClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
